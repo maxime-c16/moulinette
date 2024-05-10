e48b6e60a45c1e5b3ca44f30b2aedf5e8e4fc054
maxime <maxime.cauchy93@gmail.com>
2024-05-10 09:50:52 +0200

    split testers in testerX

--- srcs/c00/ex00/tester1.c
+++ srcs/c00/ex00/tester1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tester.c                                           :+:      :+:    :+:   */
+/*   tester1.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/09 14:19:55 by mcauchy           #+#    #+#             */
-/*   Updated: 2024/05/09 14:21:57 by mcauchy          ###   ########.fr       */
+/*   Created: 2024/05/09 13:34:19 by mcauchy           #+#    #+#             */
+/*   Updated: 2024/05/10 09:15:10 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,7 @@
 
 int	main(void)
 {
-	int	int_max;
-	int	int_min;
-	
-	int_max = 2147483647;
-	int_min = -2147483648;
-	ft_is_negative(int_max);
-	ft_is_negative(int_min);
-	ft_is_negative(0);
-	ft_is_negative(-1);
-	ft_is_negative(1);
+	ft_putchar('a');
+	ft_putchar(' ');
 	return (0);
 }
--- srcs/c00/ex04/tester5.c
+++ srcs/c00/ex04/tester5.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tester.c                                           :+:      :+:    :+:   */
+/*   tester5.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/09 13:34:19 by mcauchy           #+#    #+#             */
-/*   Updated: 2024/05/09 13:40:41 by mcauchy          ###   ########.fr       */
+/*   Created: 2024/05/10 09:46:41 by mcauchy           #+#    #+#             */
+/*   Updated: 2024/05/10 09:46:47 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,6 @@
 
 int	main(void)
 {
-	ft_putchar('a');
-	ft_putchar(' ');
-	ft_putchar(48);
-	ft_putchar(' ');
-	ft_putchar('Z');
-	ft_putchar(' ');
-	ft_putchar(127);
-	ft_putchar(' ');
-	ft_putchar(128);
-	ft_putchar(' ');
-	ft_putchar(255);
+	ft_is_negative(1);
 	return (0);
 }
