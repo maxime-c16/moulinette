171c56d7b6ca7c56b772ddb079a76f25b702fcad
maxime <maxime.cauchy93@gmail.com>
2024-05-10 10:39:05 +0200

    Refactor main.c to fix file path concatenation issue (atoi->itoa)

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 14:45:24 by mcauchy           #+#    #+#             */
-/*   Updated: 2024/05/10 10:37:11 by mcauchy          ###   ########.fr       */
+/*   Updated: 2024/05/10 10:38:22 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,7 @@ FILE	*get_student_output(char *function_dir, char *function_name, int index, int
 	if (define_v > 1)
 	{
 		command = ft_strjoin(command, "tester");
-		command = ft_strjoin(command, ft_atoi(index));
+		command = ft_strjoin(command, ft_itoa(index));
 		out_path = ft_strjoin("student_output", ft_itoa(index));
 	}
 	else
