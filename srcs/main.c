59448b02a4a6eb28a1904306444d8976120784ed
maxime <maxime.cauchy93@gmail.com>
2024-05-10 11:29:22 +0200

    Refactor main.c to fix output file path concatenation issue with index + 1

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 14:45:24 by mcauchy           #+#    #+#             */
-/*   Updated: 2024/05/10 11:27:17 by mcauchy          ###   ########.fr       */
+/*   Updated: 2024/05/10 11:28:58 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -229,7 +229,7 @@ int	create_compare_stud_output(char *function_dir, char *function_name)
 	while (i < define_v)
 	{
 		printf("Test %d: ", i + 1);
-		if (check_output(expected_output[i], student_outputs[i], output_name[i]) == 0)
+		if (check_output(expected_output[i], student_outputs[i], output_name[i], i) == 0)
 			printf("\033[0;32mOK\033[0m\n");
 		else
 		{
