aa9a0f2dfefb7c91939ea44a5c17549c6497d260
maxime <maxime.cauchy93@gmail.com>
2024-05-10 11:47:24 +0200

    Refactor main.c to clean stdout

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 14:45:24 by mcauchy           #+#    #+#             */
-/*   Updated: 2024/05/10 11:41:16 by mcauchy          ###   ########.fr       */
+/*   Updated: 2024/05/10 11:45:56 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,20 +128,17 @@ int check_output(FILE *expected_output, FILE *student_output, char *output_name,
 	{
 		if (fscanf(student_output, "%c", &student_char) == EOF)
 		{
-			printf("Error: student output is shorter than expected output\t");
 			system(command);
 			return (EXIT_FAILURE);
 		}
 		if (expected_char != student_char)
 		{
-			printf("Error: student output is different from expected output\t");
 			system(command);
 			return (EXIT_FAILURE);
 		}
 	}
 	if (fscanf(student_output, "%c", &student_char) != EOF)
 	{
-		printf("Error: student output is longer than expected output\t");
 		system(command);
 		return (EXIT_FAILURE);
 	}
